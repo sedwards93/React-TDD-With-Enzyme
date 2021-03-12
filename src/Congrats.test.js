2f8c257d86968dda8c4f8bf922d0d34cb8396022
Symion Edwards <symion.edwards@gmail.com>
2021-03-12 17:34:13 +0000

    Abstracted the adapter setup to the setupTest.js file

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { shallow } from "enzyme";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
 
-Enzyme.configure({ adapter: new Adapter() });
 const defaultProps = { success: false };
 /**
  * @function setup
