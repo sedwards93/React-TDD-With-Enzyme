78287ece465b5acfaba09e83c10417f05e258e9d
Symion Edwards <symion.edwards@gmail.com>
2021-03-12 14:48:32 +0000

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+test("renders learn react link", () => {
+  render(<App />);
+});
