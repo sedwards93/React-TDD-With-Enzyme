c16e3a218ce3ab0687b41c87ff1f313bdcf7ce23
Symion Edwards <symion.edwards@gmail.com>
2021-03-12 16:45:43 +0000

    Create test for success props are true

---
+++ test/testUtils.js
@@ -0,0 +1,10 @@
+/**
+ * Return node(s) with the given data-test attribute.
+ * @param {ShallowWrapper} wrapper
+ * @param {string} value
+ * @returns {ShallowWrapper}
+ */
+
+export const findByTestAttr = (wrapper, value) => {
+  return wrapper.find(`[data-test="${value}"]`);
+};
