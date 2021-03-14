ec31b5ccaca38911010f6ce50ea215580d780298
Symion Edwards <symion.edwards@gmail.com>
2021-03-14 11:03:16 +0000

    Start tests for guessedwords component

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,9 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const GuessedWords = (props) => {
-  return <div></div>;
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">Try to guess the secret word! </span>
+    );
+  }
+  return <div data-test="component-guessed-words"> {contents}</div>;
 };
 
-GuessedWords.propTypes = {};
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 export default GuessedWords;
