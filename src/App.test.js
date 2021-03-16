40b4be69fa04724b4c7a7111cae70aa8528009a3
Symion Edwards <symion.edwards@gmail.com>
2021-03-16 12:43:35 +0000

    Add bootstrap styling

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,4 @@ import App from "./App";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test("renders learn react link", () => {
-  render(<App />);
-});
+test("renders learn react link", () => {});
